459d7b9951becdc21af51a2bd1ace324d443205f
Logan Abernathy <MaximumUltimateDestroyer@gmail.com>
2023-07-02 03:56:49 -0700

    2.1: Changed splashes, Custom splashes from July 4th, December 25th, Rephrased the about me section.

--- src/random.js
+++ src/random.js
@@ -1,16 +1,29 @@
 window.addEventListener('DOMContentLoaded', function() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'splashes.txt', true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var splashes = xhr.responseText.split('\n').filter(Boolean);
-      shuffleArray(splashes);
-      typewrite(splashes);
-    }
-  };
-  xhr.send();
+  var date = new Date();
+  var month = date.getMonth() + 1; // January is 0
+  var day = date.getDate();
+
+  if (month === 7 && day === 4) { // Check if it's July 4th
+    var splashes = ["Happy Independence Day!"]; // Create an array with only the desired splash text
+    typewrite(splashes); // Call the typewrite function with the modified splash text
+  if (month === 12 && day === 25) {
+  var splashes = ["Merry Christmas!"];
+    typewrite(splashes);
+  } else {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'splashes.txt', true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var splashes = xhr.responseText.split('\n').filter(Boolean);
+        shuffleArray(splashes);
+        typewrite(splashes);
+      }
+    };
+    xhr.send();
+  }
 });
 
+// The rest of the code remains unchanged
 function shuffleArray(array) {
   for (var i = array.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
@@ -37,7 +50,7 @@ function typewrite(lines) {
       currentLine = currentLine.slice(0, -1);
       typewriteElement.textContent = currentLine;
     } else {
-      timeout = isDeleting ? 1000 : 1200; // Pause for 1 seconds after deletion, 5 seconds after line display
+      timeout = isDeleting ? 1000 : 1200; // Pause for 1 second after deletion, 5 seconds after line display
       isDeleting = !isDeleting;
 
       if (!isDeleting) {
